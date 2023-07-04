e4487cce081e7132a77bb0fc403dc11778c334ca
F4bian-pacheco <fabian.paxe@gmail.com>
2023-07-04 16:08:16 -0400

    misc: maquetando el formulario de respuesta

--- pages/dashboard/consultas/responder/[id].tsx
+++ pages/dashboard/consultas/responder/[id].tsx
@@ -2,12 +2,33 @@ import React from 'react'
 import { useRouter } from 'next/router'
 import ContentDashboard from '../../../../components/hocs/ContentDashboard'
 
+// añadir formulario usando react-hook-form
+const RespuestaContainerComponent = () => {
+  const [data, setData] = React.useState("")
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setData(event.target.respuesta.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="respuesta">Respuesta</label>
+        <textarea name="respuesta" id="respuesta" cols={30} rows={10}></textarea>
+        <button type="submit">Enviar</button>
+      </form>
+      <p>{data}</p>
+    </div>
+  )
+}
+
 function Respuesta() {
   const router = useRouter()
   const { id } = router.query
   return (
     <ContentDashboard titulo={"respuestas"}>
-      Respuesta {id}
+      <RespuestaContainerComponent />
     </ContentDashboard>
   )
 }
